7d163e1e415d2ea0d03641e5d277dec8afb22a1a
Joao Andrade <jjadonotenter@gmail.com>
2013-10-20 19:05:55 +0100

    Initial commit

---
+++ test/facebook-test-user_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var facebook-test-user = require('../lib/facebook-test-user.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['facebook-test-user'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(facebook-test-user.awesome(), 'awesome', 'should be awesome.');
+    test.done();
+  }
+};
